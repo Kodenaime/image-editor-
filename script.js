bd8ccfa87e28cf0311396c808d6a7d3aeb922463
iamdaniel <creativedannyk@gmail.com>
2022-08-02 11:03:24 +0100

    Revert "added some unnecessary JS"
    This reverts commit eb69932ddb28e3353f48206eb88462512d8c6104.

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let menu = document.querySelector('#menu-btn');
-let nav = document.querySelector('.nav');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    nav.classList.toggle('active');
-
-}
